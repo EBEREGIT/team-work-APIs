e7078362afa67fb99f1fd41978c17c37d0a3fda3
EBEREGIT <samsoneberenjoku@gmail.com>
2020-01-17 20:31:31 +0100

    removed test

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,21 +1,21 @@
-/* eslint-disable import/newline-after-import */
-const request = require('request');
-const baseUrl = 'http://localhost:3000/';
+// /* eslint-disable import/newline-after-import */
+// const request = require('request');
+// const baseUrl = 'http://localhost:3000/';
 
-describe('Server', () => {
-  describe('GET /', () => {
-    it('returns status code 200', (done) => {
-      request.get(baseUrl, (error, response, back) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-    });
+// describe('Server', () => {
+//   describe('GET /', () => {
+//     it('returns status code 200', (done) => {
+//       request.get(baseUrl, (error, response, back) => {
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//     });
 
-    it('returns Welcome to Team Work', (done) => {
-      request.get(baseUrl, (error, response, body) => {
-        expect(body).toBe('Welcome to Team Work');
-        done();
-      });
-    });
-  });
-});
+//     it('returns Welcome to Team Work', (done) => {
+//       request.get(baseUrl, (error, response, body) => {
+//         expect(body).toBe('Welcome to Team Work');
+//         done();
+//       });
+//     });
+//   });
+// });
