23abed0c9901ac715f973652cef112a97afdfde3
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-23 02:15:46 +0100

    added a few comments to explain the db.js properly

--- services/db.js
+++ services/db.js
@@ -1,23 +1,24 @@
-/* eslint-disable quotes */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
-const pg = require("pg");
-require("dotenv").config();
+const pg = require('pg');
+require('dotenv').config();
 
-const isProduction = process.env.NODE_ENV === "production";
+// set production variable. This will be called when deployed to a live host
+const isProduction = process.env.NODE_ENV === 'production';
 
 // configuration details
-
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-// display message on success
+// if project has been deployed, connect with the host's DATABASE_URL
+// else connect with the local DATABASE_URL
 const pool = new pg.Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 });
 
-pool.on("connect", () => {
-  console.log("Teamwork Database connected successfully!");
+// display message on success message if successful
+pool.on('connect', () => {
+  console.log('Teamwork Database connected successfully!');
 });
 
 // create tables here
@@ -154,8 +155,8 @@ const createTables = () => {
     });
 };
 
-pool.on("remove", () => {
-  console.log("client removed");
+pool.on('remove', () => {
+  console.log('client removed');
   process.exit(0);
 });
 
@@ -164,4 +165,4 @@ module.exports = {
   pool,
 };
 
-require("make-runnable");
+require('make-runnable');
