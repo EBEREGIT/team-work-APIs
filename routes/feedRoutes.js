26dbfeea9c776fee1abfa74869e55d5dd5a15dc4
EBEREGIT <samsoneberenjoku@gmail.com>
2020-05-29 09:35:19 +0100

    added auth to the feed API

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -2,10 +2,11 @@
 /* eslint-disable import/newline-after-import */
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const viewAllFeed = require('./feedRoutes/viewAllFeed');
 
 // view all feeds
-router.get('/', viewAllFeed.viewAllFeed);
+router.get('/', auth, viewAllFeed.viewAllFeed);
 
 module.exports = router;
